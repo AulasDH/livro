6a4828baaac60501c4a1f1a0cf895baa7c468f28
tsunodajapa <gabrieltsunoda@gmail.com>
2022-05-04 20:36:37 -0300

    feat: buscando livros

--- src/models/Livro.js
+++ src/models/Livro.js
@@ -33,6 +33,7 @@ module.exports = (sequelize, DataTypes) => {
     },
     {
       tableName: 'livros',
+      timestamps: false,
     }
   );
 
---
+++ src/routes/livros.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const livrosController = require('../controllers/livrosController');
+
+const router = express.Router();
+
+router.get('/', livrosController.index);
+
+module.exports = router;
