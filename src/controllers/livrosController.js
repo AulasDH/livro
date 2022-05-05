c06ca1a4645231f68437890fb7f198d3a4747ef6
tsunodajapa <gabrieltsunoda@gmail.com>
2022-05-04 21:25:20 -0300

    feat: atualiza um livro

--- src/controllers/livrosController.js
+++ src/controllers/livrosController.js
@@ -5,7 +5,33 @@ const livrosController = {
         const livros = await Livro.findAll();
 
         return response.json(livros);
-    }
+    },
+    update: async (request, response) => {
+        const { idLivro } = request.params;
+        const payload = request.body;
+
+        /**
+         * @description Método 1 - Atualiza todos parametros
+            const qtslivrosAtualizados = await Livro.update(payload, {
+                where: {
+                    id: idLivro
+                },
+            });
+
+            const livro = await Livro.findOne({ where: { id: idLivro } });
+        */
+
+        /**
+         * @description Método 2 - Atualiza os parametros que foram realmente modificados
+        */
+        const livro = await Livro.findOne({ where: { id: idLivro } });
+
+        Object.assign(livro, payload);
+
+        await livro.save();
+
+        return response.json(livro);
+    },
 };
 
 module.exports = livrosController;
--- src/routes/livros.js
+++ src/routes/livros.js
@@ -4,6 +4,10 @@ const livrosController = require('../controllers/livrosController');
 
 const router = express.Router();
 
+// /livros
 router.get('/', livrosController.index);
+
+// /livros/2
+router.put('/:idLivro', livrosController.update);
 
 module.exports = router;
